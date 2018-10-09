25c5abd8946f82180c897d287613298079956da4
wz <296215200@qq.com>
2018-10-09 21:19:20 +0800

    design patterns

--- src/test/java/com/enjoy/dp/Decorator/example4/Example4Test.java
+++ src/test/java/com/enjoy/dp/Decorator/example4/Example4Test.java
@@ -13,13 +13,13 @@ import java.io.*;
  */
 public class Example4Test {
     @Test
-    public void example4Test() {
+    public void example4Test1() {
     //public static void main(String[] args) {
         //流式输出文件
         DataOutputStream dout = null;
         try {
             dout = new DataOutputStream(
-                    new EncryptOutputStream(
+                  new EncryptOutputStream2(
                             new BufferedOutputStream(
                                     new FileOutputStream("MyEncrypt.txt"))));
         } catch (FileNotFoundException e) {
@@ -31,6 +31,26 @@ public class Example4Test {
             dout.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void example4Test2() throws Exception{
+        //流式读取文件
+        DataInputStream din = null;
+        try{
+            din = new DataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream("MyEncrypt.txt")
+                    )
+            );
+            //然后就可以获取文件内容了
+            byte bs []= new byte[din.available()];
+            din.read(bs);
+            String content = new String(bs);
+            System.out.println("文件内容===="+content);
+        }finally{
+            din.close();
         }
     }
 }
